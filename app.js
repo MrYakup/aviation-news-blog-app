5ad6262c213fc6b54c14bb12947f0cda8d894409
yakup <yakup@gmail.com>
2022-04-10 12:32:28 +0300

    Ready For Heroku

--- app.js
+++ app.js
@@ -1,15 +1,65 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const ejs = require('ejs');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageControllers');
 
+
+
+
+/*********EXPRESS İ BİR DEĞİŞKENE ATIYORUZ***************/
 const app = express();
+/*******************************************************/
 
-app.use(express.static('public'))
 
-app.get('/', (req,res) =>{
-    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+/******connect DB - mongo veri tabanına bağlanmak için*****/
+mongoose.connect('mongodb+srv://yakup:İzmir7704.@cluster0.utjzo.mongodb.net/article-db?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>{
+  console.log('DB CONNECTED!')
+}).catch((err)=>{
+  console.log(err)
 })
+/*********************************************************/
 
-const port = 3000;
+/****************TEMPLATE ENGINE**************************/
+app.set('view engine', 'ejs')
+/*********************************************************/
+
+
+/*****************MIDDLEWARES****************************/
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
+/**********************************************************/
+
+
+/**********************ROUTES******************************/
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto); //görselimizi,başlığımızı,descriptionumuzu yani formdaki verileri yakalayıp yazdırmak için kullandığmız kod
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+
+
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
+
+// app.get('/video-page', (req, res) => {
+//   res.render('video-page');
+// });
+/**********************************************************/
+
+
+/****************SUNUCUYU DİNLEMEK İÇİN********************/
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`sunucu ${port} portunda başlatıldı.`);
 });
+/**********************************************************/
---
+++ controllers/photoControllers.js
@@ -0,0 +1,78 @@
+const Photo = require('../models/Photo');
+const fs = require('fs');
+
+
+exports.getAllPhotos = async (req, res) => {
+
+  const page = req.query.page || 1;
+  const photosPerPage = 3;
+  const totalPhotos = await Photo.find().countDocuments();
+  // console.log(totalPhotos);
+  const photos = await Photo.find({}).sort('-dateCreated').skip((page-1)*photosPerPage).limit(photosPerPage)
+
+
+   res.render('index', {
+    photos: photos,
+    current: page,
+    pages: Math.ceil(totalPhotos / photosPerPage)
+  });
+
+  // res.sendFile(path.resolve(__dirname,'views/index.html'))
+  // const photos = await Photo.find({}).sort('-dateCreated');
+  // res.render('index', {
+  //   photos,
+  // });
+  
+};
+
+
+exports.getPhoto = async (req, res) => {
+  // console.log(req.params.id)
+  // res.render('about');
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+};
+
+
+exports.updatePhoto = async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  await photo.save();
+  res.redirect(`/photos/${req.params.id}`);
+};
+
+
+exports.deletePhoto = async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/../public' + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect('/');
+};
+
+
+exports.createPhoto = async (req, res) => {
+  // console.log(req.files.image)
+  // console.log(req.body);
+  // await Photo.create(req.body)
+  // res.redirect('/')
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/../public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
+};
